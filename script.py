06009c028801be3c4e09b004698be2e71921ee43
sebashsiao <matth@Matts-iMac.local>
2023-10-10 15:23:50 -0500

    Initial commit

---
+++ script.py
@@ -0,0 +1,8 @@
+# We have hidden code in another file. If you're curious, open the folder to the left and inspect the simulate.py file
+
+from simulate import simulate
+
+# defalut num people = 2
+num_people_in_room = 20 			#Change This Number (keep it smaller than 100 to save processing power)
+
+simulate(num_people_in_room)
